86d01f1ce4837cf927486fedfb29e35e7dcf88b2
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-09-10 20:17:00 +0200

    Refactoring of the ACL (client side - wip).

--- txconsole-web/src/main/java/net/txconsole/web/resource/Resource.java
+++ txconsole-web/src/main/java/net/txconsole/web/resource/Resource.java
@@ -3,7 +3,6 @@ package net.txconsole.web.resource;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import net.txconsole.core.security.SecurityFunction;
-import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.ResourceSupport;
 
@@ -17,8 +16,7 @@ public class Resource<T> extends ResourceSupport {
     public static final String REL_GUI = "gui";
 
     private final T data;
-    @JsonSerialize(using = SecurityFunctionSetSerializer.class)
-    private final Set<SecurityFunction> actions = new HashSet<>();
+    private final Set<String> actions = new HashSet<>();
 
     public Resource<T> withLink(Link link) {
         add(link);
@@ -26,7 +24,7 @@ public class Resource<T> extends ResourceSupport {
     }
 
     public Resource<T> withAction(SecurityFunction action) {
-        actions.add(action);
+        actions.add(action.getCategory().name() + "#" + action.name());
         return this;
     }
 
