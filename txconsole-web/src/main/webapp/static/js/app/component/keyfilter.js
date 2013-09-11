119b99072f729ef31b4275ee7b3daa4f1bad890a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-09-11 11:43:27 +0200

    Key filter component (wip)

---
+++ txconsole-web/src/main/webapp/static/js/app/component/keyfilter.js
@@ -0,0 +1,11 @@
+define([], function () {
+
+    function filterKey (config) {
+        alert(config.filter)
+    }
+
+    return {
+        filterKey: filterKey
+    }
+
+});
--- txconsole-web/src/main/webapp/static/js/app/component/request.js
+++ txconsole-web/src/main/webapp/static/js/app/component/request.js
@@ -1,4 +1,4 @@
-define(['dialog', 'ajax', 'application', 'jcombo', 'jconfigurable'], function (dialog, ajax, application, jcombo, jconfigurable) {
+define(['dialog', 'ajax', 'application', 'jcombo', 'jconfigurable', 'component/keyfilter'], function (dialog, ajax, application, jcombo, jconfigurable, keyfilter) {
 
     /**
      * Creating a request for a branch
@@ -13,12 +13,28 @@ define(['dialog', 'ajax', 'application', 'jcombo', 'jconfigurable'], function (d
                     templateId: 'request-create',
                     data: requestConfigurationData,
                     initFn: function (dialog) {
-                        txExchangeJCombo = jcombo.init(dialog.get('#request-txfileexchange'), {
+                        // Selection of the file exchange
+                        txExcFIXMEhangeJCombo = jcombo.init(dialog.get('#request-txfileexchange'), {
                             url: 'ui/ref/txfileexchange',
                             extension: jconfigurable.jcomboExtension({
                                 path: 'txfileexchange'
                             })
-                        })
+                        });
+                        // Key filter on RETURN
+                        dialog.get('#request-key-filter').keydown(function (e) {
+                            if (e.which == 13) {
+                                keyfilter.filterKey({
+                                    filter: dialog.get('#request-key-filter').val()
+                                });
+                                e.preventDefault();
+                            }
+                        });
+                        // Key filter on the Search button
+                        dialog.get('#request-key-filter-go').click(function () {
+                            keyfilter.filterKey({
+                                filter: dialog.get('#request-key-filter').val()
+                            });
+                        });
                     }
                 })
             }
