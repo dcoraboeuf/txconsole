bd28f892360f30205420be0ed9fe3b6cf041e86f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-10-02 16:13:37 +0200

    #22 Removing an ACL (wip - HTTP 404 on delete)

--- txconsole-web/src/main/webapp/static/js/app/controller/acl-project.js
+++ txconsole-web/src/main/webapp/static/js/app/controller/acl-project.js
@@ -1,10 +1,23 @@
-define(['render'], function (render) {
+define(['render', 'jquery', 'ajax', 'dynamic'], function (render, $, ajax, dynamic) {
 
     return {
         url: function (config) {
             return 'ui/acl/project/{0}'.format(config.project);
         },
-        render: render.asTableTemplate('acl-project')
+        render: render.asTableTemplate('acl-project', function (config) {
+            $('.acl-delete').each(function (i, action) {
+                var project = $(action).attr('data-project');
+                var account = $(action).attr('data-account');
+                $(action).click(function () {
+                    ajax.del({
+                        url: '/ui/acl/project/{0}/{1}'.format(project, account),
+                        successFn: function () {
+                            dynamic.reloadSection('acl-project');
+                        }
+                    })
+                })
+            });
+        })
     }
 
 });
