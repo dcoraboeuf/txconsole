5209837c9a2032688d7c52672966e5473c9dbe52
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-10-03 10:07:53 +0200

    #29 Moves the buttons to the request-status component

--- txconsole-web/src/main/webapp/static/js/app/controller/request-status.js
+++ txconsole-web/src/main/webapp/static/js/app/controller/request-status.js
@@ -1,8 +1,31 @@
-define(['render', 'component/request'], function (render) {
+define(['jquery', 'render', 'component/request'], function ($, render) {
     return {
         url: function (config) {
             return 'ui/request/{0}'.format(config.requestId)
         },
-        render: render.asSimpleTemplate('request-status')
+        render: render.asSimpleTemplate('request-status', render.sameDataFn, function () {
+
+            $('#request-hide-deleted').click(function() {
+                if($(this).hasClass('active')) {
+                    $('.translation-entry-type-DELETED').show();
+                } else {
+                    $('.translation-entry-type-DELETED').hide();
+                }
+            });
+
+            $('#request-hide-valid').click(function() {
+                if($(this).hasClass('active')) {
+                    // if filter on deleted already active, avoid to display them.
+                    if($('#request-hide-deleted').hasClass('active')) {
+                        $('#request-view').find('tr:not(.translation-entry-invalid)').not('.translation-entry-type-DELETED').show();
+                    } else {
+                        $('#request-view').find('tr:not(.translation-entry-invalid)').show();
+                    }
+                } else {
+                    $('#request-view').find('tr:not(.translation-entry-invalid)').hide();
+                }
+            });
+
+        })
     }
 })
