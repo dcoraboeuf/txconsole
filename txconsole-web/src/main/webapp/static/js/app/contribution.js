b42f85813112a577e0ed8eb8f7653b205e693a7c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-10-07 13:30:36 +0200

    #33 Edition (wip)

--- txconsole-web/src/main/webapp/static/js/app/contribution.js
+++ txconsole-web/src/main/webapp/static/js/app/contribution.js
@@ -1,7 +1,64 @@
-define(['jquery', 'ajax', 'render', 'handlebars'], function ($, ajax, render) {
+define(['jquery', 'ajax', 'render', 'handlebars', 'jquery.typing'], function ($, ajax, render) {
 
     var projectId = $('#project').val();
     var branchId = $('#branch').val();
+
+    var contributions = [];
+
+    function saveEdition(field) {
+        // Gets the data
+        var bundle = field.attr('data-bundle');
+        var section = field.attr('data-section');
+        var key = field.attr('data-key');
+        var locale = field.attr('data-locale');
+        var oldValue = field.attr('data-old-value');
+        var value = field.val();
+        if (value != oldValue) {
+            // Sends the data to the contributions
+            contributions.push({
+                bundle: bundle,
+                section: section,
+                key: key,
+                locale: locale,
+                value: value
+            });
+            // TODO Updates the contribution view
+            // Clears the field status
+            field.removeClass('contribution-field-ongoing');
+        }
+    }
+
+    function prepareEditionField(field) {
+        // Changes
+        field.typing({
+            start: function () {
+                field.addClass('contribution-field-ongoing');
+            }
+        });
+        // Special keys
+        field.keydown(function (e) {
+            // On Shit+Enter ==> submit
+            if (e.which == 13 && e.shiftKey) {
+                // Sends the changes
+                saveEdition(field);
+                // Does not send the normal event
+                e.preventDefault();
+            }
+            // On escape, cancels the changes
+            if (e.which == 27) {
+                // Restores the old value
+                field.val(field.attr('data-old-value'));
+                // Removes the edition class
+                field.removeClass('contribution-field-ongoing');
+                // Does not send the normal event
+                e.preventDefault();
+            }
+        });
+        // Exiting the field
+        field.blur(function (e) {
+            saveEdition(field)
+        })
+    }
 
     function prepareResults() {
         // Hiding locales
@@ -10,6 +67,10 @@ define(['jquery', 'ajax', 'render', 'handlebars'], function ($, ajax, render) {
             $(action).click(function () {
                 $('.locale-{0}'.format(locale)).hide();
             })
+        });
+        // Edition
+        $('.contribution-field').each(function (i, field) {
+            prepareEditionField($(field));
         })
     }
 
