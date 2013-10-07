ac69e6e419fa004030b213c43c5a8d775ea71653
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-10-07 16:45:19 +0200

    #41 Mock implementation for the backend integration tests

---
+++ txconsole-backend/src/test/java/net/txconsole/backend/support/MockResourceService.java
@@ -0,0 +1,26 @@
+package net.txconsole.backend.support;
+
+import net.txconsole.core.model.BranchSummary;
+import net.txconsole.core.model.ProjectSummary;
+import net.txconsole.core.model.Resource;
+import net.txconsole.service.ResourceService;
+import org.springframework.hateoas.Link;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+
+@Service
+public class MockResourceService implements ResourceService {
+
+    @Override
+    public Resource<BranchSummary> getBranch(Locale locale, BranchSummary branch) {
+        return new Resource<>(branch)
+                .withLink(new Link("http://server:port/context/branch/" + branch.getId(), Resource.REL_GUI));
+    }
+
+    @Override
+    public Resource<ProjectSummary> getProject(Locale locale, ProjectSummary project) {
+        return new Resource<>(project)
+                .withLink(new Link("http://server:port/context/project/" + project.getId(), Resource.REL_GUI));
+    }
+}
