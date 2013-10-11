664400605dfa0f1ff5338403a20708c599158e2f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-10-11 15:39:48 +0200

    #40 Correction of the event query URL

--- txconsole-web/src/main/webapp/static/js/app/controller/events.js
+++ txconsole-web/src/main/webapp/static/js/app/controller/events.js
@@ -3,7 +3,7 @@ define(['render'], function (render) {
     return {
         url: function (config) {
             if (config.entity) {
-                return 'ui/events?{0}={1}'.format(config.entity, config.entityId)
+                return 'ui/events?entity={0}&entityId={1}'.format(config.entity, config.entityId)
             } else {
                 return 'ui/events?u=1'
             }
